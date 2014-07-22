4023b051273a0c58c82bcbbadbd7f68446cf6293
2x2.4raymon <2x2.4raymon@gmail.com>
2014-07-22 14:37:07 +0400

    Try to commit one more files

--- src/YC.Utils.SourceText/AssemblyInfo.fs
+++ src/YC.Utils.SourceText/AssemblyInfo.fs
@@ -57,8 +57,8 @@ open System.Runtime.InteropServices
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.1.6500.6")]
-[<assembly: AssemblyVersion("0.1.6500.5")>]
-[<assembly: AssemblyFileVersion("0.1.6500.5")>]
+// [assembly: AssemblyVersion("0.1.*")]
+[<assembly: AssemblyVersion("0.1.6500.4")>]
+[<assembly: AssemblyFileVersion("0.1.6500.4")>]
 
 do ()
