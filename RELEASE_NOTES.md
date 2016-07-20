64945674244e185a587f5e269215dc52632716b3
rsdpisuy <rsdpisuy@gmail.com>
2016-07-20 10:01:07 +0300

    Bump version to 0.3.0.1

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,7 @@
+### 0.3.0.1 - 20 July 2016
+* Latest version of FSharp.Core
+* Data structure for positions in regular set parsing. 
+
 ### 0.3.0.0 - 20 July 2016
 * Latest version of FSharp.Core
 * Data structure for positions in regular set parsing. 
--- src/YC.Utils.SourceText/AssemblyInfo.fs
+++ src/YC.Utils.SourceText/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("YC.Utils.SourceText")>]
 [<assembly: AssemblyProductAttribute("YC.Utils.SourceText")>]
 [<assembly: AssemblyDescriptionAttribute("Helpers for source code basic manipulation.")>]
-[<assembly: AssemblyVersionAttribute("0.3.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.3.0.0")>]
+[<assembly: AssemblyVersionAttribute("0.3.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.0.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.3.0.0"
-    let [<Literal>] InformationalVersion = "0.3.0.0"
+    let [<Literal>] Version = "0.3.0.1"
+    let [<Literal>] InformationalVersion = "0.3.0.1"
