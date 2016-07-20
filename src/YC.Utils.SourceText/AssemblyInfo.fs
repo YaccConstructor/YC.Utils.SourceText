2af947b1a349cc478b574392cd6a2f8a2f3fea45
rsdpisuy <rsdpisuy@gmail.com>
2016-07-20 09:54:20 +0300

    Bump version to 0.3.0.0

--- src/YC.Utils.SourceText/AssemblyInfo.fs
+++ src/YC.Utils.SourceText/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("YC.Utils.SourceText")>]
 [<assembly: AssemblyProductAttribute("YC.Utils.SourceText")>]
 [<assembly: AssemblyDescriptionAttribute("Helpers for source code basic manipulation.")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.3.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
-    let [<Literal>] InformationalVersion = "1.0"
+    let [<Literal>] Version = "0.3.0.0"
+    let [<Literal>] InformationalVersion = "0.3.0.0"
